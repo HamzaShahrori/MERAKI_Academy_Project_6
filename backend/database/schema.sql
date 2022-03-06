cb76934db681ae1f5f6ebd7447b274a9adfac61a
AnasNaamneh <naamnih.anas@gmail.com>
2022-03-06 20:12:37 +0300

    create database

---
+++ backend/database/schema.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE MERAKI_Academy_Project_6;
+
+USE MERAKI_Academy_Project_6;
+
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
+const connection = require("./database/db")
 
 app.use(cors());
 
