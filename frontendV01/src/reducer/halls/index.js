3799af11bdc06f47d50e14d2f4f375ba7ca05d43
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-03-08 22:04:40 +0300

    create redux

---
+++ frontendV01/src/reducer/halls/index.js
@@ -0,0 +1,55 @@
+const initialState = {
+  halls: [],
+};
+const hallsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case "SET_HALLS":
+      return { ...state, halls: payload };
+
+    case "ADD_HALLS":
+      return { ...state, halls: [...state.halls, payload] };
+
+    case "UPDATE_HALLS":
+      return {
+        ...state,
+        halls: state.halls.map((element) => {
+          if (payload.id == element.id) {
+            return payload;
+          }
+          return element;
+        }),
+      };
+
+    case "DELETE_HALLS":
+      return {
+        ...state,
+        halls: state.halls.filter((halls) => {
+          return halls.id != payload;
+        }),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default hallsReducer;
+
+export const setHalls = (halls) => {
+  return {
+    type: "SET_HALLS",
+    payload: halls,
+  };
+};
+
+export const AddHall = (newHall) => {
+  return { type: "ADD_HALLS", payload: newHall };
+};
+
+export const updateHalls = (updateHall) => {
+  return { type: "UPDATE_HALLS", payload: updateHall };
+};
+
+export const deleteHalls = (id) => {
+  return { type: "DELETE_HALLS", payload: id };
+};
