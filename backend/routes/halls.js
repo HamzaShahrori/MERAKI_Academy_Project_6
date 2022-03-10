4e1c3cd906a14e4ae329a2c18a5e0a64be51d9b9
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-03-10 13:44:12 +0300

    create getHallByUserId in backend

--- backend/routes/halls.js
+++ backend/routes/halls.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-const { createNewHall,getAllHalls,getHallById,updateHallById,deleteHallById,getHallsByAddress,updateThePriceAfterDiscount,getHallsByDiscount } = require("../controllers/halls");
+const { createNewHall,getAllHalls,getHallById,updateHallById,deleteHallById,getHallsByAddress,updateThePriceAfterDiscount,getHallsByDiscount,getHallByUserId } = require("../controllers/halls");
 
 const hallsRouter = express.Router();
 
-hallsRouter.post("/", createNewHall);
+hallsRouter.post("/:user_id", createNewHall);
 hallsRouter.get("/page", getAllHalls);
 hallsRouter.get("/:id", getHallById);
 hallsRouter.put("/:id", updateHallById);
@@ -12,6 +12,7 @@ hallsRouter.delete("/:id", deleteHallById);
 hallsRouter.get("/page/hall_address", getHallsByAddress);
 hallsRouter.put("/", updateThePriceAfterDiscount);
 hallsRouter.get("/page/Home", getHallsByDiscount);
+hallsRouter.get("/add/:user_id", getHallByUserId);
 
 
 
