163a088a16a2818483a8145d2a04b972f843eb8e
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-03-18 23:11:56 +0300

    add booking table

--- frontendV01/src/component/bookingTable/Booking.js
+++ frontendV01/src/component/bookingTable/Booking.js
@@ -1,83 +1,124 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Booking.css";
+import { MdPayment } from "react-icons/md";
+import { IoIosTime } from "react-icons/io";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import {
+  BsFillCalendarDateFill,
+  BsTelephoneOutboundFill,
+  BsFillCalendarDayFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import { setHall } from "../../reducer/halls";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams, NavLink } from "react-router-dom";
 const BookingTable = () => {
-  const [reserver, setReserver] = useState("");
-  const [booking_day, setBooking_day] = useState("");
-  const [date_booking, setDate_booking] = useState("");
-  const [booking_time, setBooking_time] = useState("");
-  const [phone, setPhone] = useState("");
-  const [payment, setPayment] = useState("");
-
   const state = useSelector((state) => {
     return {
       token: state.loginReducer.token,
-      halls: state.hallsReducer.halls,
+      hallById: state.hallsReducer.hallById,
     };
   });
+  const Navigate = useNavigate();
   const dispatch = useDispatch();
   const { halls_id } = useParams();
   const getBookingById = () => {
     axios
-      .get(`http://localhost:5000/booking/all/${halls_id}`, {
-        headers: { Authorization: `Bearer ${state.token}` },
-      })
+      .get(
+        `http://localhost:5000/booking/all/${localStorage.getItem("hall-id")}`,
+        {
+          headers: { Authorization: `Bearer ${state.token}` },
+        }
+      )
       .then((result) => {
         console.log(result.data.result);
+
         dispatch(setHall(result.data.result));
       });
   };
+  console.log(state.hallById);
   useEffect(() => {
     getBookingById();
   }, []);
-
+  const convert = () => {
+    Navigate(`/details/${localStorage.getItem("hall-id")}`);
+  };
   return (
     <>
-      {state.halls &&
-        state.halls.map((element, i) => {
-          <div key={i}>
-            <p>{element.reserver}</p>;<p>{element.booking_day}</p>;
-            <p>{element.booking_time}</p>;<p>{element.phone}</p>;
-            <p>{element.payment}</p>;
-          </div>;
-        })}
-      <div className="div3">
-        <menu
-          className="main-menu"
-          style={{ marginTop: "10%", marginLeft: "3%" }}
-        >
-          <div className="main-menu__top">
-            {/* <SiBitdefender className="logo"></SiBitdefender> */}
-          </div>
-          <br />
-          <nav className="main-menu_nav" id="menu2">
-            <NavLink className="link1" to="/details-booking/:user_id">
-              New Hall
-            </NavLink>
-            <br />
-            <br />
-            <br />
-            <NavLink className="link1" to="/wedding-Hall">
-              Wedding Halls
-            </NavLink>
-            <br />
-            <br />
-            <br />
+      <div>
+        <table className="head">
+          <tr className="table1">
+            <th className="id1">Id</th>
+            <th className="name1">
+              <MdDriveFileRenameOutline style={{ fontSize: "1.3em" }} /> Name{" "}
+            </th>
+            <th className="day1">
+              <BsFillCalendarDayFill /> Day
+            </th>
+            <th className="date1">
+              <BsFillCalendarDateFill /> Date
+            </th>
+            <th className="phone1">
+              <BsTelephoneOutboundFill /> Phone
+            </th>
+            <th className="time1">
+              <IoIosTime style={{ fontSize: "1.3em" }} /> Time{" "}
+            </th>
+            <th className="payment1">
+              <MdPayment style={{ fontSize: "1.3em" }} /> Payment
+            </th>
+          </tr>{" "}
+          {state.hallById &&
+            state.hallById.map((element, i) => (
+              <tr>
+                <td className="id">{element.id}</td>
+                <td className="name">{element.reserver}</td>
+                <td className="day">{element.booking_day}</td>
+                <td className="date"> {element.date_booking}</td>
+                <td className="phone"> {element.phone}</td>
+                <td className="time">{element.booking_time} AM </td>
+                <td className="donation">{element.payment}</td>
+              </tr>
+              //
+            ))}
+        </table>
 
-            <NavLink className="link1" to="/details/:halls_id">
-              Booking Tables
-            </NavLink>
+        <div className="div3">
+          <menu
+            className="main-menu"
+            style={{ marginTop: "10%", marginLeft: "3%" }}
+          >
+            <div className="main-menu__top">
+              {/* <SiBitdefender className="logo"></SiBitdefender> */}
+            </div>
             <br />
-            <br />
-            <br />
-            {/* <NavLink className="blog" to="/blog">
+            <nav className="main-menu_nav" id="menu2">
+              <NavLink className="link1" to="/details-booking/:user_id">
+                New Hall
+              </NavLink>
+              <br />
+              <br />
+              <br />
+              <NavLink className="link1" to="/wedding-Hall">
+                Wedding Halls
+              </NavLink>
+              <br />
+              <br />
+              <br />
+
+              <a className="link1" onClick={() => convert()}>
+                Booking Tables
+              </a>
+              <br />
+              <br />
+              <br />
+              {/* <NavLink className="blog" to="/blog">
             
             </NavLink> */}
-          </nav>
-        </menu>
+            </nav>
+          </menu>
+        </div>
       </div>
     </>
   );
