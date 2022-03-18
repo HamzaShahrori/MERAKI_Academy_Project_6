971345ea95e36c54f308bcdf58178ed37315d522
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-03-18 23:11:08 +0300

    add icons

--- frontendV01/src/component/NewHalls/New.js
+++ frontendV01/src/component/NewHalls/New.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import axios from "axios";
 import "./New.css";
 import { NavLink } from "react-router-dom";
@@ -121,152 +121,147 @@ const New = ({ num, setNum, search }) => {
   useEffect(() => {
     getHallByUserId();
   }, []);
+  const convert = () => {
+    Navigate(`/details/${localStorage.getItem("hall-id")}`);
+  };
   return (
     <div id="all">
-   
-
       {/*--- create new hall --- */}
       <div className="div2">
-      <form style={{ marginLeft: "30%" }}>
-        <fieldset>
-          <div className="mb-3">
-            <div className="form-floating" style={{ width: "300px" }}>
-              <input
-                type="text"
-                className="form-control"
-                id="floatingInputGrid"
-                placeholder="Hall Name"
-                onChange={(e) => setHall_name(e.target.value)}
-              />
-              <label for="floatingInputGrid">Hall Name</label>
-            </div>
-          </div>
-          <div className="mb-3">
-            <div className="form-floating" style={{ width: "300px" }}>
-              <textarea
-                type="text"
-                className="form-control"
-                id="floatingInputGrid"
-                placeholder="Hall Name"
-                onChange={(e) => setHall_description(e.target.value)}
-              />
-              <label for="floatingInputGrid"> Description</label>
-            </div>
-          </div>
-          <div className="row g-1">
-            <div className="col-md">
+        <form style={{ marginLeft: "30%" }}>
+          <fieldset>
+            <div className="mb-3">
               <div className="form-floating" style={{ width: "300px" }}>
                 <input
-                  type="number"
+                  type="text"
                   className="form-control"
                   id="floatingInputGrid"
-                  placeholder="Hall Price"
-                  onChange={(e) => setPrice(e.target.value)}
+                  placeholder="Hall Name"
+                  onChange={(e) => setHall_name(e.target.value)}
                 />
-                <label for="floatingInputGrid">Hall Price</label>
+                <label for="floatingInputGrid">Hall Name</label>
               </div>
             </div>
-          </div>
-          <br></br> <div className="dis">
-          <div
-            className="col-md"
-            style={{marginLeft:"380px", width:"300px"}}
-          >
-           
-            <div className="form-floating">
-              <input
-                type="number"
-                onChange={(e) => setDiscount(e.target.value)}
-                className="form-control"
-                id="floatingInputGrid"
-                placeholder="Discount"
-              />
-              <label for="floatingInputGrid">Discount</label>
+            <div className="mb-3">
+              <div className="form-floating" style={{ width: "300px" }}>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  id="floatingInputGrid"
+                  placeholder="Hall Name"
+                  onChange={(e) => setHall_description(e.target.value)}
+                />
+                <label for="floatingInputGrid"> Description</label>
+              </div>
             </div>
-          </div>
-          <br></br>
-          <div
-            className="col-md"
-            style={{ marginLeft: "40%" }}
-          >
-            <div className="form-floating" style={{ width: "300px" }}>
-              <input
-                type="number"
-                className="form-control"
-                id="floatingInputGrid"
-                placeholder="Discount"
-                onChange={(e) => setPriceBeforeDiscount(e.target.value)}
-              />
-              <label for="floatingInputGrid">Price Before</label>
+            <div className="row g-1">
+              <div className="col-md">
+                <div className="form-floating" style={{ width: "300px" }}>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="floatingInputGrid"
+                    placeholder="Hall Price"
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                  <label for="floatingInputGrid">Hall Price</label>
+                </div>
+              </div>
             </div>
-          </div>{" "}
-          <br></br>
-          <div
-            style={{
-              marginTop: "0%",
-              width: "300px",
-              height: "50px",
-              marginLeft: "40%",
-            }}
-            className="input-group col-mb-3"
-          >
-            <input
-              type="file"
-              onChange={(e) => {
-                setImageSelected(e.target.files[0]);
-              }}
-              className="form-control"
-              placeholder=" Video Link"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-            />
-
-            <button
-              type="button"
-              class="btn "
-              id="upload"
-              onClick={() => uploadImage(imageselected)}
-            >
-              <AiOutlineCloudUpload title="upload Image"  />{" "}
-            </button>
-          </div>{" "}
-          <br></br>
-         
-          </div>
-          <br></br>
-          <div class="mb-3" id="select" >
-            <div className="form-floating" style={{ width: "700px" }}>
-              <select
-                onChange={(e) => {
-                  setHall_address(e.target.value);
-                }}
-                style={{ marginTop: "6%", width: "300px" }}
-                className="form-select"
-                id="floatingSelect"
-                aria-label="Floating label select example"
+            <br></br>{" "}
+            <div className="dis">
+              <div
+                className="col-md"
+                style={{ marginLeft: "380px", width: "300px" }}
               >
-                <option selected>Select Address</option>
-                <option value="Amman">Amman</option>
-                <option value="Irbid">Irbid</option>
-                <option value="Zarqa">Zarqa</option>
-                <option value="Salt">Salt</option>
-                <option value="Madaba">Madaba</option>
-                <option value="Mafraq">Mafraq</option>
-                <option value="Jerash">Jerash</option>
-                <option value="Ma'an">Ma'an</option>
-                <option value="Tafila">Tafila</option>
-                <option value="Karak">Karak</option>
-                <option value="Aqapa">Aqapa</option>
-                <option value="Ajlun">Ajlun</option>
-              </select>
-              {/* <label for="floatingSelect">Select Address</label> */}
+                <div className="form-floating">
+                  <input
+                    type="number"
+                    onChange={(e) => setDiscount(e.target.value)}
+                    className="form-control"
+                    id="floatingInputGrid"
+                    placeholder="Discount"
+                  />
+                  <label for="floatingInputGrid">Discount</label>
+                </div>
+              </div>
+              <br></br>
+              <div className="col-md" style={{ marginLeft: "40%" }}>
+                <div className="form-floating" style={{ width: "300px" }}>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="floatingInputGrid"
+                    placeholder="Discount"
+                    onChange={(e) => setPriceBeforeDiscount(e.target.value)}
+                  />
+                  <label for="floatingInputGrid">Price Before</label>
+                </div>
+              </div>{" "}
+              <br></br>
+              <div
+                style={{
+                  marginTop: "0%",
+                  width: "300px",
+                  height: "50px",
+                  marginLeft: "40%",
+                }}
+                className="input-group col-mb-3"
+              >
+                <input
+                  type="file"
+                  onChange={(e) => {
+                    setImageSelected(e.target.files[0]);
+                  }}
+                  className="form-control"
+                  placeholder=" Video Link"
+                  aria-label="Username"
+                  aria-describedby="basic-addon1"
+                />
+                <button
+                  type="button"
+                  class="btn "
+                  id="upload"
+                  onClick={() => uploadImage(imageselected)}
+                >
+                  <AiOutlineCloudUpload title="upload Image" />{" "}
+                </button>
+              </div>{" "}
+              <br></br>
             </div>
-          </div>
-         
-          {/* <button type="button" btn-lg">New Hall</button> */}
-        </fieldset>
-        <br></br>
-        <div
+            <br></br>
+            <div class="mb-3" id="select">
+              <div className="form-floating" style={{ width: "700px" }}>
+                <select
+                  onChange={(e) => {
+                    setHall_address(e.target.value);
+                  }}
+                  style={{ marginTop: "6%", width: "300px" }}
+                  className="form-select"
+                  id="floatingSelect"
+                  aria-label="Floating label select example"
+                >
+                  <option selected>Select Address</option>
+                  <option value="Amman">Amman</option>
+                  <option value="Irbid">Irbid</option>
+                  <option value="Zarqa">Zarqa</option>
+                  <option value="Salt">Salt</option>
+                  <option value="Madaba">Madaba</option>
+                  <option value="Mafraq">Mafraq</option>
+                  <option value="Jerash">Jerash</option>
+                  <option value="Ma'an">Ma'an</option>
+                  <option value="Tafila">Tafila</option>
+                  <option value="Karak">Karak</option>
+                  <option value="Aqapa">Aqapa</option>
+                  <option value="Ajlun">Ajlun</option>
+                </select>
+                {/* <label for="floatingSelect">Select Address</label> */}
+              </div>
+            </div>
+            {/* <button type="button" btn-lg">New Hall</button> */}
+          </fieldset>
+          <br></br>
+          <div
             className="input-group col-mb-3"
             style={{
               width: "300px",
@@ -288,53 +283,55 @@ const New = ({ num, setNum, search }) => {
 
             <button
               type="button"
-              class="btn" id="upload" 
+              class="btn"
+              id="upload"
               onClick={() => uploadVideo(imageselected)}
             >
               {" "}
-              <AiOutlineCloudUpload title="upload Video"/>{" "}
+              <AiOutlineCloudUpload title="upload Video" />{" "}
             </button>
           </div>
           <br></br> <br></br>
-          <button type="button"  id="button"  onClick={addNewHall}>
+          <button type="button" id="button" onClick={addNewHall}>
             New Hall
           </button>
-      </form>
+        </form>
       </div>
       <div className="div1">
-      <menu
-        className="main-menu"
-        style={{ marginTop: "10%", marginLeft: "3%" }}
-      >
-        <div className="main-menu__top">
-          {/* <SiBitdefender className="logo"></SiBitdefender> */}
-        </div>
-        <br />  
-        <nav className="main-menu_nav" id="menu">
-          <NavLink className="link1" to="/details-booking/:user_id">
-            New Hall
-          </NavLink>
+        <menu
+          className="main-menu"
+          style={{ marginTop: "10%", marginLeft: "3%" }}
+        >
+          <div className="main-menu__top">
+            {/* <SiBitdefender className="logo"></SiBitdefender> */}
+          </div>
           <br />
-          <br />
-          <br />
-          <NavLink className="link1" to="/wedding-Hall">
-            Wedding Halls
-          </NavLink>
-          <br />
-          <br />
-          <br />
+          <nav className="main-menu_nav" id="menu">
+            <NavLink className="link1" to="/details-booking/:user_id">
+              New Hall
+            </NavLink>
+            <br />
+            <br />
+            <br />
+            <NavLink className="link1" to="/wedding-Hall">
+              Wedding Halls
+            </NavLink>
+            <br />
+            <br />
+            <br />
 
-          <NavLink className="link1" to="/details/:halls_id">
-            Booking Tables
-          </NavLink>
-          <br />
-          <br />
-          <br />
-          {/* <NavLink className="blog" to="/blog">
+            <a className="link1" onClick={() => convert()}>
+              Booking Tables
+            </a>
+            <br />
+            <br />
+            <br />
+            {/* <NavLink className="blog" to="/blog">
             
             </NavLink> */}
-        </nav>
-      </menu></div>
+          </nav>
+        </menu>
+      </div>
     </div>
   );
 };
